2106a1a07e7fc75e3c8e2577d0ab7ddd767e1c2d
Đỗ Khoa <momentofm3@gmail.com>
2019-07-11 13:22:41 +0700

    cut element

---
+++ nuochoa/admin/footer.php
@@ -0,0 +1,34 @@
+<footer class="main-footer">
+      <strong>Copyright &copy; 2014-2018 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+      All rights reserved.
+      <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.0.0-alpha
+      </div>
+    </footer> 
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="./assets/plugins/jquery/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="./assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- FastClick -->
+  <script src="./assets/plugins/fastclick/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="./assets/js/adminlte.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+</body>
+
+</html>
