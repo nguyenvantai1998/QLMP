fd3e6db90167135b5343bdefada381664ac68964
NguyenVanTai <vantain124@gmail.com>
2019-07-11 14:48:51 +0700

    update

---
+++ nuochoa/admin/Query/admin_select.php
@@ -0,0 +1,21 @@
+
+<?php
+
+
+
+
+function query_select($sql)
+{
+    $table = array();
+    try {
+        include 'connect.php';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $table = $stmt;
+        $conn = null;
+    } catch (PDOException $e) {
+        echo "connection failed: " . $e->getMessage();
+    }
+    return $table;
+}
+?>
---
+++ nuochoa/admin/indexAdmin.php
@@ -0,0 +1,47 @@
+
+  <!-- header -->
+  <?php include("header.php"); ?>
+
+    <!-- Navbar -->
+    <?php include("navbar.php"); ?>
+  
+    <!-- side Bar -->
+    <?php include("sideBar.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard v2</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+
+          <!-- FORM ADD PRODUCT -->
+    
+          <?php include("./Add/product.php"); ?>
+          <!-- END FORM -->
+
+
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+    </div>
+
+  <!-- Footer -->
+  <?php include("footer.php"); ?>
